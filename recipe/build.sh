a415b73d5bebcc57433c54a20141e8ab40976943
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-09-29 17:36:23 +0200

    made ect-cli.sh/.command executable

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,12 @@ $PYTHON setup.py install
 if [ `uname` == Darwin ]
 then
     cp $RECIPE_DIR/ect-cli.command $PREFIX/bin/
+    chmod 775 $PREFIX/bin/ect-cli.command
     cp -r $RECIPE_DIR/ect-cli.app $PREFIX/bin/
     sed -i -e "s,\${PKG_VERSION},${PKG_VERSION},g"  "${PREFIX}/bin/ect-cli.app/Contents/Info.plist"
 else
     cp $RECIPE_DIR/ect-cli.sh $PREFIX/bin
+    chmod 775 $PREFIX/bin/ect-cli.sh
     mkdir -p  $PREFIX/share/ect
     cp $RECIPE_DIR/ect.desktop-template $PREFIX/share/ect/
     cp $RECIPE_DIR/ect.png $PREFIX/share/ect/
