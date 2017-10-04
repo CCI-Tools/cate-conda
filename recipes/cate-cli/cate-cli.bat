915babdf589b743cfe2bee4f9f051702973dc2b6
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2017-10-04 18:08:49 +0200

    fix windows cli bat

--- recipes/cate-cli/cate-cli.bat
+++ recipes/cate-cli/cate-cli.bat
@@ -12,13 +12,21 @@ call "%CATE_BIN%\activate.bat" "%CATE_HOME%"
 if errorlevel 1 exit 1
 
 if "%*" == "" (
-  prompt $G$S
-
-  echo.
-  @echo ESA CCI Toolbox (CLI) command-line interface. Type "cate -h" to get help.
-  echo.
-
-  cmd /K ""
+  goto INTERACTIVE
 ) else (
-  %*
+  goto DELEGATE
 )
+
+:INTERACTIVE
+prompt $G$S
+echo.
+@echo ESA CCI Toolbox (CLI) command-line interface. Type "cate -h" to get help.
+echo.
+
+cmd /K ""
+exit 0
+
+:DELEGATE
+%*
+exit 0
+
