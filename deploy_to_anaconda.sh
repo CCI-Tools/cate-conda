9994b774470e83c2a8200fa4b000b7a21bac470b
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-11-21 11:23:19 +0100

    - This is a travis test. Hence, set the travis branch to all - Fixed deployment - renamed deployment file to make clearer what it does

--- deploy_to_anaconda.sh
+++ deploy_to_anaconda.sh
@@ -5,6 +5,6 @@ set -e
 echo "mode: $1"
 echo
 
-CONDA_PACKAGE=$(conda build -c conda-forge recipes/$1 --output);
+# CONDA_PACKAGE=$(conda build -c conda-forge recipes/$1 --output);
 echo "conda package ${CONDA_PACKAGE}. OS: ${TRAVIS_OS_NAME}";
 echo anaconda -v -t ${ANACONDA_TOKEN} upload "${CONDA_PACKAGE}" -u ccitools --force;
