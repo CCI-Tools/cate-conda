b27b3d22c02597ce4933bb5c5b4f72ce18f2c4c7
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-11-21 14:04:36 +0100

    - Fixed deploy sript - Made deploy script OS sensitive

--- deploy_to_anaconda.sh
+++ deploy_to_anaconda.sh
@@ -7,9 +7,16 @@ echo
 
 if [[ "${TRAVIS_OS_NAME}" = "linux" ]]
 then
-    CONDA_PACKAGE=$(conda build -c conda-forge recipes/$1 --output);
+    CONDA_PACKAGE=$(conda build -c conda-forge recipes/cate --output);
     echo "conda package ${CONDA_PACKAGE}. OS: ${TRAVIS_OS_NAME}";
-    echo anaconda -v -t ${ANACONDA_TOKEN} upload "${CONDA_PACKAGE}" -u ccitools --force;
+
+    if [[ "$1" = "production" ]]
+    then
+        echo anaconda -v -t ${ANACONDA_TOKEN} upload "${CONDA_PACKAGE}" -u ccitools --force;
+    else
+        echo "Deployment would looke like:"
+        echo anaconda -v -t ${ANACONDA_TOKEN} upload "${CONDA_PACKAGE}" -u ccitools --force;
+    fi
 else
     echo "Anaconda upload is only executed when running on linux"
 fi
