64c37aa8262e7bf25c0f891ed02351c30c408334
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-11-21 13:37:09 +0100

    Fixing deploy script

--- deploy_to_anaconda.sh
+++ deploy_to_anaconda.sh
@@ -5,6 +5,11 @@ set -e
 echo "mode: $1"
 echo
 
-# CONDA_PACKAGE=$(conda build -c conda-forge recipes/$1 --output);
-echo "conda package ${CONDA_PACKAGE}. OS: ${TRAVIS_OS_NAME}";
-echo anaconda -v -t ${ANACONDA_TOKEN} upload "${CONDA_PACKAGE}" -u ccitools --force;
+if [[ "${TRAVIS_OS_NAME}" = "linux" ]]
+then
+    CONDA_PACKAGE=$(conda build -c conda-forge recipes/$1 --output);
+    echo "conda package ${CONDA_PACKAGE}. OS: ${TRAVIS_OS_NAME}";
+    echo anaconda -v -t ${ANACONDA_TOKEN} upload "${CONDA_PACKAGE}" -u ccitools --force;
+else
+    echo "Anaconda upload is only executed when running on linux"
+fi
